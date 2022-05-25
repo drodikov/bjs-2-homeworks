1c1fd31814f620b11196b54768f13e0cf1f7b2c7
Дмитрий Родиков <drodikov@mail.ru>
2022-05-25 21:18:50 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,5 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    arr = [];    
+  } else if (d == 0) {
+    let x = -b / (2 * a);
+    arr = [x];
+  } else {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [x1, x2];
+  }
+
   // код для задачи №1 писать здесь
   return arr; // array
 }
