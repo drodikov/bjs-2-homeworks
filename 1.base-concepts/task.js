48e6fea8d56edd85789a95ac0cc005293ff0d970
Дмитрий Родиков <drodikov@mail.ru>
2022-05-26 13:18:04 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,8 +19,35 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
+  if (typeof percent === 'string') {
+    percent = Number(percent);
+  } else if (typeof percent === 'number') {
+    percent = percent;
+  } else {
+    console.log(`Параметр процентная ставка содержит неправильное значение ${percent}`);
+  }
+  if (typeof contribution === 'string') {
+    contribution = Number(contribution);
+  } else if (typeof contribution === 'number') {
+    contribution = contribution;
+  } else {
+    console.log(`Параметр сумма первоначального взноса содержит неправильное значение ${contribution}`);
+  }
+  if (typeof amount === 'string') {
+    amount = Number(amount);
+  } else if (typeof amount === 'number') {
+    amount = amount;
+  } else {
+    console.log(`Параметр сумма кредита содержит неправильное значение ${amount}`);
+  }
+  let s = amount - contribution;
+  let p = percent / 12;
+  let date1 = new Date();
+  let n = (date - date1) / 2592000000;
+  let e = s * (p + (p / (((1 + p) ** n) - 1)));
+  totalAmount = e * n.toFixed(2);
+  console.log(totalAmount);
+    // код для задачи №2 писать здесь
 
   return totalAmount;
 }
